37d7482740a6b7e7b028b06efe2f88611953d0ea
abdullah-al-ryan <abdullah15-13088@diu.edu.bd>
2023-06-15 14:53:21 +0600

    data modeling

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      require: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const user = mongoose.model('User', userSchema);
+
+export default user;
