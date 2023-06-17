83570ceea4c9a890a9ea258439f7825ff8836a40
abdullah-al-ryan <abdullah15-13088@diu.edu.bd>
2023-06-17 13:28:37 +0600

    product controller added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,32 +1,11 @@
 import express from 'express';
 const router = express.Router();
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
-
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    // We are getting all the products from the database by the find{} empty object method.
-    const products = await Product.find({});
-    throw new Error('This is an error');
-    res.json(products);
-  })
-);
-
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      return res.json(product);
-    } else {
-      // if there is no product, we will return a 404 status code and a message.
-      res.status(404);
-      throw new Error('Resource not found');
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
