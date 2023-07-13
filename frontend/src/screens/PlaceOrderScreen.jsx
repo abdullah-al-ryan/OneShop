75be0cf646371ab92054144be6cf7b5a1dad5197
abdullah-al-ryan <abdullah15-13088@diu.edu.bd>
2023-07-13 11:49:20 +0600

    order screen added

---
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import {Button, Row, Col, ListGroup, Image, Card} from 'react-bootstrap';
+import CheckoutSteps from '../components/CheckoutSteps';
+
+const PlaceOrderScreen = () => {
+    const navigate = useNavigate();
+    const cart = useSelector(state => state.cart);
+    
+    useEffect(() => {
+        if(!cart.shippingAddress){
+            navigate('/shipping');
+        }
+        else if(!cart.paymentMethod){
+            navigate('/payment');
+        }
+    }, [cart.paymentMethod, cart.shippingAddress.address, navigate]);
+
+  return (
+    <>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>Column 1</Col>
+        <Col md={4}>Column 2</Col>
+      </Row>
+    </>
+  )
+}
+
+export default PlaceOrderScreen
