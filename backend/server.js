9bf57a9c50682d70a6a22a36ffd42716be092d1e
abdullah-al-ryan <abdullah15-13088@diu.edu.bd>
2023-07-13 11:21:51 +0600

    order routes & controller added

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,14 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 const port = process.env.PORT || 5000;
 
 connectDB(); // connect to MongoDB
 
 const app = express();
-
+3
 //Body parser middleware. These two lines of middleware allow us to accept JSON data in the body.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,6 +28,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
