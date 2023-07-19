1b79e87b5e0a7fd9f255363f6ad5de94af33fa74
abdullah-al-ryan <abdullah15-13088@diu.edu.bd>
2023-07-19 10:45:10 +0600

    display order history added

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -5,9 +5,11 @@ import { Table, Button, Row, Col,Form } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
+import { FaTimes } from 'react-icons/fa';
 import { toast } from 'react-toastify';
 import { useProfileMutation } from '../slices/usersApiSlice';
 import { setCredentials } from '../slices/authSlice';
+import { useGetMyOrdersQuery } from '../slices/ordersApiSlice';
 
 const ProfileScreen = () => {
     const [name, setName] = useState('');
@@ -21,6 +23,8 @@ const ProfileScreen = () => {
     const { userInfo } = useSelector((state) => state.auth);
 
     const [updateProfile, { isLoading:loadingUpdateProfile}] = useProfileMutation();
+
+    const {data: orders, isLoading, error} = useGetMyOrdersQuery();
 
     useEffect(() => {
         if (userInfo) {
@@ -100,9 +104,51 @@ const ProfileScreen = () => {
                 {loadingUpdateProfile && <Loader />}
             </Form>
         </Col>
-        <Col md={9}>Column</Col>
-    </Row>
-  )
-}
+        <Col md={9}>
+            <h2>My Orders</h2>
+            {isLoading ? <Loader /> : error ?  (
+                <Message variant='danger'>{error?.data?.message || error.error}</Message>
+            )   : ( 
+                <Table striped hover responsive className='table-sm'>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Date</th>
+                            <th>Total</th>
+                            <th>Paid</th>
+                            <th>Delivered</th>
+                            <th></th>
+                        </tr>
+                    </thead>
 
-export default ProfileScreen
+                    <tbody>
+                        {orders?.map((order) => (
+                            <tr key={order._id}>
+                                <td>{order._id}</td>
+                                <td>{order.createdAt.substring(0, 10)}</td>
+                                <td>${order.totalPrice}</td>
+                                <td>{order.isPaid ? order.paidAt.substring(0, 10) : (
+                                    <FaTimes style={{color:'red'}} />
+                                )}</td>
+
+                                <td>{order.isDelivered ? order.deliveredAt.substring(0, 10) : (
+                                    <FaTimes style={{color:'red'}} />
+                                    )}
+                                </td>
+                                    <LinkContainer to={`/order/${order._id}`}>
+                                        <Button className='btn-sm' variant='light'>Details</Button>
+                                    </LinkContainer>
+                                <td>
+
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            ) }
+        </Col>
+    </Row>
+  );
+};
+
+export default ProfileScreen;
