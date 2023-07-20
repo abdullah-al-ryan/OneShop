5739cf0dc14ffa716e5cc3184944a12ced580413
abdullah-al-ryan <abdullah15-13088@diu.edu.bd>
2023-07-20 15:48:40 +0600

    created reviews

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -86,10 +86,53 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc   Create a new review
+// @route  POST /api/products/:id/reviews
+// @access Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      // check if the user has already reviewed the product
+      (review) => review.user.toString() === req.user._id.toString()
+    );
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error('Product already reviewed');
+    }
+
+    const review = {
+      // create a new review
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review); // add the new review to the product's reviews array
+
+    product.numReviews = product.reviews.length; // update the number of reviews
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length; // update the rating
+
+    await product.save(); // save the product
+    res.status(201).json({ message: 'Review added' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
 export {
   getProducts,
   getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 };
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -11,11 +11,12 @@ import {
   Button,
   ListGroupItem,
 } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Rating from '../components/Rating';
+import {toast} from 'react-toastify';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
-import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
+import { useGetProductDetailsQuery, useCreateReviewMutation } from '../slices/productsApiSlice';
 import { addToCart } from '../slices/cartSlice';
 
 const ProductScreen = () => {
@@ -27,13 +28,37 @@ const ProductScreen = () => {
 
   const [qty, setQty] = useState(1); // qty is the quantity of the product that we want to add to the cart, we are setting it to 1 by default, we will change it later on the product screen, we will have a select dropdown for the quantity,
 
-  const { data: product, isLoading, error } = useGetProductDetailsQuery(productId); // productId is coming because of the use params hook
+  const [rating, setRating] = useState(0); // rating is the rating that the user will give to the product, we are setting it to 0 by default, we will change it later on the product screen, we will have a select dropdown for the rating,
+
+  const [comment, setComment] = useState(''); // comment is the comment that the user will give to the product, we are setting it to an empty string by default, we will change it later on the product screen, we will have a select dropdown for the comment,
+
+  const { data: product, isLoading, refetch,  error } = useGetProductDetailsQuery(productId); // productId is coming because of the use params hook
+
+  const [ createReview, { isLoading: loadingProductReview } ] = useCreateReviewMutation(); // we are destructuring the createReview function from the useCreateReviewMutation hook, we are also destructuring the isLoading and error properties from the useCreateReviewMutation hook, we are setting the default value of isLoading to isLoadingReview and the default value of error to errorReview
+
+  const {userInfo} = useSelector((state) => state.auth); // we are destructuring the userInfo object from the userLogin property of the state, this will give us the user info of the logged in user
 
   const addToCartHandler = () => {
     dispatch(addToCart({ ...product, qty })); // we are dispatching the addToCart action creator, we are passing the product and the quantity
 
     navigate('/cart'); // we are navigating to the cart screen
   };
+
+  const submitHandler = async (e) => {
+    e.preventDefault(); 
+    try{
+        await createReview({ productId, rating, comment }).unwrap(); // we are calling the createReview function, we are passing the productId, rating and comment as arguments, we are awaiting the createReview function because it is an async function
+      
+      refetch();
+      toast.success('Review Submitted');
+      setRating(0);
+      setComment('');
+    }
+      catch(err){
+        toast.error(err?.data?.message || err.error);
+    }
+  }
+    
 
 
   return (
@@ -47,6 +72,7 @@ const ProductScreen = () => {
       ) : error ? (
         <Message variant='danger'>{ error?.data?.message || error.error }</Message>
       ) :  (
+      <>
         <Row>
         <Col md={5}>
           <Image src={product.image} alt={product.name} fluid />
@@ -126,7 +152,73 @@ const ProductScreen = () => {
             </ListGroup>
           </Card>
         </Col>
-      </Row>
+        </Row>
+        
+        <Row>
+          <Col md={6}>
+            <h2>Reviews</h2>
+            {product.reviews.length === 0 && <Message>No Reviews</Message>}
+            <ListGroup variant='flush'>
+              {product.reviews.map((review) => (
+                <ListGroupItem key={review._id}>
+                  <strong>{review.name}</strong>
+                  <Rating value={review.rating}></Rating>
+                  <p>{review.createdAt.substring(0, 10)}</p>
+                  <p>{review.comment}</p>
+                </ListGroupItem>
+              ))}
+              <ListGroupItem>
+                <h2>Write a Customer Review</h2>
+
+                {loadingProductReview && <Loader></Loader>}
+                {error && <Message variant='danger'>{ error?.data?.message || error.error }</Message>}
+
+                {userInfo ? (
+                  <Form onSubmit={submitHandler}>
+                    <Form.Group controlId='rating' className='my-2'>
+                      <Form.Label>Rating</Form.Label>
+                      <Form.Control
+                        as='select'
+                        value={rating}  
+                        onChange={(e) => setRating(Number(e.target.value))}
+                      >
+                        <option value=''>Select...</option>
+                        <option value='1'>1 - Poor</option>
+                        <option value='2'>2 - Fair</option>
+                        <option value='3'>3 - Good</option>
+                        <option value='4'>4 - Very Good</option>
+                        <option value='5'>5 - Excellent</option>
+                      </Form.Control>
+                    </Form.Group>
+
+                    <Form.Group controlId='comment' className='my-2'>
+                      <Form.Label>Comment</Form.Label>
+                      <Form.Control
+                        as='textarea'
+                        row='3'
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                      ></Form.Control>
+                    </Form.Group>
+                    <Button
+                      type='submit'
+                      variant='primary'
+                      disabled={loadingProductReview}
+                    >
+                      Submit
+                    </Button>
+                  </Form>
+                ) : (
+                  <Message>
+                    Please <Link to='/login'>sign in</Link> to write a review{' '}
+                  </Message>
+                )}
+
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+        </Row>
+      </>
       ) } 
 
       
